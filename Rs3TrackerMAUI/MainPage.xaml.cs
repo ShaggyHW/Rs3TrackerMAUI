e7b41eff8c1ae5616ecc0beb4e7b285e6aa3e30b
ShaggyHW <danieltneves@gmail.com>
2023-10-04 22:43:43 +0100

    MainWindow

--- Rs3TrackerMAUI/MainPage.xaml.cs
+++ Rs3TrackerMAUI/MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Maui.Dispatching;
-using SharpHook;
-using SharpHook.Reactive;
+
 
 namespace Rs3TrackerMAUI;
 
@@ -13,27 +12,39 @@ public partial class MainPage : ContentPage {
     }
 
     private void MainPage_Loaded(object sender, EventArgs e) {
-        var hook = new TaskPoolGlobalHook();
-        hook.KeyPressed += Hook_KeyPressed;
-
-        hook.RunAsync();
+       
     }
 
-    private void Hook_KeyPressed(object sender, KeyboardHookEventArgs e) {
-        MainThread.BeginInvokeOnMainThread(() => {
-            CounterBtn.Text = e.Data.KeyCode.ToString();
-        });
+    private void btnAbilityConfig_Clicked(object sender, EventArgs e) {
+
     }
 
-    private void OnCounterClicked(object sender, EventArgs e) {
-        count++;
+    private void btnBars_Clicked(object sender, EventArgs e) {
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+    }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    private void btnSettings_Clicked(object sender, EventArgs e) {
+
+    }
+
+    private void btnServer_Clicked(object sender, EventArgs e) {
+
+    }
+
+    private void btnClose_Clicked(object sender, EventArgs e) {
+
+    }
+
+    private void btnStartTracker_Clicked(object sender, EventArgs e) {
+
+    }
+
+    private void CanResize_CheckedChanged(object sender, CheckedChangedEventArgs e) {
+
+    }
+
+    private void cmbMode_SelectedIndexChanged(object sender, EventArgs e) {
+
     }
 }
 
